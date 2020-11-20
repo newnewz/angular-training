a4c3bd686f59e52648e5ce560f0df364519322c5
Xulu <xulu1F42F@gmail.com>
2020-11-19 16:48:33 -0800

    lesson updates

---
+++ my-first-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: [`
+  p {
+    color: dodgerBlue;
+  }`]
+})
+export class AppComponent {
+  
+}
